4eadfef6160738ff44584f701d3fc8057b6d360c
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-01 23:31:59 -0700

    added static route to app.js

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ let express = require('express');
 let app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
