3488063ba7eccfc35b5fcc0eab180f935a707cc3
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-02 18:41:29 -0700

    added POST request example

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get(`/users/2`, (req, res) => {
   res.send({ id: 2, login: `Nancy`, age: 20 });
+});
+
+app.post(`/users`, (req, res) => {
+  console.log(req.body);
+  res.send(`Created!`);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,24 @@
 let btn = document.querySelector('.btn');
+let postBtn = document.querySelector('.post');
 
 btn.addEventListener(`click`, () => {
   fetch(`http://localhost:3000/users/2`)
     .then(resp => resp.json())
+    .then(data => console.log(data));
+});
+
+postBtn.addEventListener('click', () => {
+  fetch(`http://localhost:3000/users`, {
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'text/plain'
+    },
+    body: JSON.stringify({
+      login: `Nancy`,
+      age: 20
+    })
+  })
+    .then(resp => resp.text())
     .then(data => console.log(data));
 });
